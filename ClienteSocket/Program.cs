3d29139e54dac67a61bef42c6506915b2f6da1e4
gbalejandro <alejandrobright@gmail.com>
2018-10-04 18:31:54 -0500

    Commit inicial del cliente socket para el centro de pagos.

--- ClienteSocket/Program.cs
+++ ClienteSocket/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace ClienteSocket
     {
         static void Main(string[] args)
         {
+            Enviar();
+        }
+
+        private static void Enviar()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            string datosAEnviar = "hola Alex";
+            string ipDestino = "127.0.0.1";
+            int puertoDestino = 8000;
+            byte[] datosEnBytes = Encoding.Default.GetBytes(datosAEnviar);
+            EndPoint ipPuertoRemoto = new IPEndPoint(IPAddress.Parse(ipDestino), puertoDestino);
+            socket.SendTo(datosEnBytes, ipPuertoRemoto);
+            //si ya tienes un EndPoint como por ejemplo el de quien  
+            //te ha enviado datos, entonces puedes usar ese en el método SendTo 
         }
     }
 }
