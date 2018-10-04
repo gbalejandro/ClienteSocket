9d741edffc8e2c467371af1ad698e7c80b0bb8e6
gbalejandro <alejandrobright@gmail.com>
2018-10-04 18:18:57 -0500

    Add project files.

---
+++ ClienteSocket/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
